6eea0deb1525d6e93187cc8ba7f639c5a246fc59
diego-watanabe <dwatanabe314@gmail.com>
2022-10-25 00:04:45 -0400

    use env variables for access token

--- my_dagster_project/assets/__init__.py
+++ my_dagster_project/assets/__init__.py
@@ -1,14 +1,21 @@
 from dagster import asset
-from github import Github 
+from github import Github
+
+import pandas as pd
+from datetime import timedelta
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import pickle
+import jupytext
+
+from github import InputFileContent
 
 ACCESS_TOKEN = "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"
 
 @asset
 def github_stargazers():
-    return list(Github(ACCESS_TOKEN).get_repo("dagster-io/dagster"))
-
-import pandas as pd
-from datetime import timedelta
+    return list(Github(ACCESS_TOKEN).get_repo("dagster-io/dagster").get_stargazers_with_dates())
 
 @asset
 def github_stargazers_by_week(github_stargazers):
@@ -16,38 +23,32 @@ def github_stargazers_by_week(github_stargazers):
         [
             {
                 "users": stargazer.user.login,
-                "week": stargazer.starred_at - timedelta(days=stargazer.starred_at.weekday()),
+                "week": stargazer.starred_at.date()
+                + timedelta(days=6 - stargazer.starred_at.weekday()),
             }
             for stargazer in github_stargazers
         ]
     )
     return df.groupby("week").count().sort_values(by="week")
 
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
-import pickle
-import jupytext
-
 @asset
 def github_stars_notebook(github_stargazers_by_week):
     markdown = f"""
 # Github Stars
 
-```python
-import pickle
-github_stargazers_by_week = pickle.loads({pickle.dumps(github_stargazers_by_week)!r})
+    ```python
+    import pickle
+    github_stargazers_by_week = pickle.loads({pickle.dumps(github_stargazers_by_week)!r})
+    ```
 
 ## Github Stars by Week, last 52 weeks
-
-github_stargazers_by_week.tail(52).reset_index().plot.bar(x="week", y="users")
-
+    ```python
+    github_stargazers_by_week.tail(52).reset_index().plot.bar(x="week", y="users")
+    ```
     """
-    nb = jupytext.reads(markdown, fmt="md")
-    ExecutePreprocessor().preprocess(nb, {})
+    nb = jupytext.reads(markdown, "md")
+    ExecutePreprocessor().preprocess(nb)
     return nbformat.writes(nb)
-
-
-from github import InputFileContent
 
 @asset
 def github_stars_notebook_gist(context, github_stars_notebook):
